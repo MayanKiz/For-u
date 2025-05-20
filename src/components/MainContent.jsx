53e869f1a054c1d788319c9bcc46e7075039122b
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-05-20 12:18:49 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -60,6 +60,7 @@ export default function MainContent() {
                     { date: 'September, 2023', event: 'Our Journey Began', emoji: '❤️' },
                     { date: 'Science Exhibition', event: 'Favorite time spent with you', emoji: '🫂' },
                     { date: '3 September, 2023', event: 'First day we talked', emoji: '💬' },
+                    { date: '5 January, 2025', event: 'Exact 12 baje, sabse khaas wish apkii thi…”', emoji: '⏰' },
                     { date: '5 January, 2025', event: 'First Trip Together', emoji: '🛺' },
                     { date: 'Many times', event: 'First Fight', emoji: '😅' },
                     { date: 'Dance Event on Teachers Day', event: 'First Topic of Conversation', emoji: '🤔' },
